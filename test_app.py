21a9b84ca948e0a9d626752c4cd323dfabb81786
yhmwpy <1182626007@qq.com>
2026-06-03 17:17:58 +0800

    Add files via upload

---
+++ test_app.py
@@ -0,0 +1,7 @@
+import pytest
+from app import app
+
+def test_hello():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == "Hello, SE!"
